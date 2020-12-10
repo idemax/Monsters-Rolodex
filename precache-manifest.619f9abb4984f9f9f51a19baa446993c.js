cccf985022b5c384a634a361bb781b146e61358e
Marcelo Lourencini Filho <marcelo.f@samsung.com>
2020-12-10 10:19:03 -0400

    Updates

---
+++ precache-manifest.619f9abb4984f9f9f51a19baa446993c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a84a69bce9ac6a51d2f47b190861d4cb",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "6035a1a50a3d1cf0f7c8",
+    "url": "/Monsters-Rolodex/static/css/main.6fce4cf2.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/Monsters-Rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6035a1a50a3d1cf0f7c8",
+    "url": "/Monsters-Rolodex/static/js/main.1dc009a9.chunk.js"
+  },
+  {
+    "revision": "f79345753b7bd810d938",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.a9daa82d.js"
+  }
+]);
